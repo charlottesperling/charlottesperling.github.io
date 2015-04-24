a4ee1590b60a23a9e808d61cfedf1410ad4c9cf5
charlottesperling <charlottemsperling@gmail.com>
2015-04-24 12:41:00 -0700

    Header disappear / reappear on scroll

--- index.js
+++ index.js
@@ -15,6 +15,61 @@ $(document).ready(function () {
 });
 	});
 
-// Jquery gets rid of .intro class styling for the Intro section - doesn't acknowledge the margins and padding?
 // Possible to break menu toggle -- menu button stays yellow if user does not close menu (toggle menu back to grey) before resizing window
 	// Need to write some sort of condition that says if menu is closed, go back to grey?
+
+
+
+//Hide header on scroll
+
+	var didScroll;
+	var lastScrollTop = 0;
+	var delta = 5;
+	var navbarHeight = $('header').outerHeight();
+	// on scroll, let the interval function know the user has scrolled
+	$(window).scroll(function(event){
+		didScroll = true;
+
+	});
+
+		//The interval checks every 250ms if didScroll has changed. 
+		//If it has, it runs the function and resets didScroll to false. 
+
+	setInterval(function(){
+		if(didScroll) {
+			hasScrolled();
+			didScroll = false;
+		}
+
+	}, 250);
+
+		function hasScrolled(){
+
+			//Store scroll position in a variable for easy access
+			var st = $(this).scrollTop();
+
+			//Check if scrolled more than delta
+			if (Math.abs(lastScrollTop - st) <= delta)
+				return;
+
+			//Check if they scrolled past the header and if they scrolled up or down
+			// If they scrolled down and are past the navbar, add class .nav-up.
+    		// This is necessary so you never see what is "behind" the navbar.
+			if (st > lastScrollTop && st > navbarHeight){
+
+				//Scroll down
+				$('header').removeClass('nav-down').addClass('nav-up');
+			}
+
+			else {
+
+				//Scroll up
+				if(st + $(window).height() < $(document).height()) {
+					$('header').removeClass('nav-up').addClass('nav-down');
+
+				}
+			}
+
+			lastScrollTop = st;
+
+		}
